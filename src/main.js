18137f89baa7e474d834cbe41c5c1b4f7f6e949a
gimhyoungsoo <gimhyoungsoo@gmail.com>
2022-06-05 16:30:08 +0900

    SPA로 포트폴리오 만들기
    이력서 view, 패럴랙스 view 만들 예정

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import router from './router'
+
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false
+
+new Vue({
+    vuetify: new Vuetify(),
+    router,
+    render: h => h(App)
+}).$mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img id="profile" alt="profile" src="../assets/profile.png">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h2>본 사이트는 node.js를 활용한 SPA 웹 사이트입니다.</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+  #profile{
+    width: 20vw;
+    border-color: black;
+  }
+  img{
+
+    width: 10vw;
+  }
+</style>
